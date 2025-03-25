46bc85130ce5e5171007cf9329d606752f67687c
manish chaudhary <honeychaudhary05021996@gmail.com>
2025-03-25 20:11:02 +0530

    craete email

--- 09_handling_api/freeapi_username.py
+++ 09_handling_api/freeapi_username.py
@@ -9,19 +9,20 @@ def user_freeapi():
         userData = data["data"]
         username = userData["login"]["username"]
         Gender = userData["gender"]
+        Email = userData["email"]
         Password = userData["login"]["password"]
         country = userData["location"]["country"]
         city = userData["location"]["city"]  
         state = userData["location"]["state"]  
-        return username, Gender, Password,  country, city, state
+        return username, Gender, Email, Password,  country, city, state
     else:
         raise Exception("Failed fetch user the data")
     
     
 def main():
     try:
-        username, gender, password,  country, city, state = user_freeapi()
-        print(f"Username: {username} \n Gender: {gender} \n Password: {password}  \n Country: {country} \n city: {city} \n state: {state}" )
+        username, gender, email, password,  country, city, state = user_freeapi()
+        print(f"Username: {username} \n Gender: {gender} \n Email: {email}\n Password: {password}  \n Country: {country} \n city: {city} \n state: {state}" )
         
     except Exception as e:
         print(str(e))  
